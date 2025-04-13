9997702addc77f5f39852daaddbc085bf7df57bb
lrnToSuccess <69418536+prasantsahu-ai@users.noreply.github.com>
2025-04-13 17:33:48 +0530

    Webscrapping with R

--- FirstProgram.R
+++ FirstProgram.R
@@ -38,3 +38,9 @@ m <- matrix(1:4, nrow=2, ncol=2)
 dimnames(m) <- list(c('a','b'), c('c','d'))
 print(m)
 
+#Web Scrapping
+con <- url("https://www.google.com",'r')
+x <- readLines(con)
+head(x)
+
+
